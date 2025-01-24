8180661ba7f2d4b4e6fdab72e4dc8bad234e20c6
UnicornIrix <iris.levrio@gmail.com>
2025-01-24 23:04:18 +0100

    errori cambio scena

---
+++ ggj/control_script.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class control_script : Control
+{
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	public void start_game(){
+
+		PackedScene scenaprova = ResourceLoader.Load<PackedScene>("res://scenaprova.tscn").Instantiate();
+		GetTree().Root.AddChild(scenaprova);
+	}
+}
