7ee9c32b8e7b6747cb252e1e4afec4bcf5d5996c
danial <daniad7@outlook.com>
2025-01-25 16:55:52 +0100

    added background

--- ggj/Assets/Enemy/Enemy.cs
+++ ggj/Assets/Enemy/Enemy.cs
@@ -13,7 +13,7 @@ public partial class Enemy : CharacterBody2D
 
 	[Export]
 	Timer timeBetweenShootTimer;
-	
+
 	[Export]
 	Timer attackSubStateTimer;
 
@@ -31,7 +31,8 @@ public partial class Enemy : CharacterBody2D
 
 	Player playerTarget;
 
-	enum States {
+	enum States
+	{
 		Normal,
 		Chase,
 		Attack
@@ -45,32 +46,36 @@ public partial class Enemy : CharacterBody2D
 		MoveAttack
 	}
 
-	AttackStates attackState; 
+	AttackStates attackState;
 
 	Vector2 attackMoveDir;
 
-    bool isOnCamera;
+	bool isOnCamera;
 
 	SpawnPointHandler spawnPointHandler;
 
-	public SpawnPointHandler SpawnPointHandler{
-		get{
+	public SpawnPointHandler SpawnPointHandler
+	{
+		get
+		{
 			return spawnPointHandler;
 		}
-		set{
+		set
+		{
 			spawnPointHandler = value;
 		}
 	}
 
-    public override void _Ready()
-    {
-        spawnPointHandler?.AddCurrentAliveEnemy();
-    }
-
-    public override void _PhysicsProcess(double delta)
+	public override void _Ready()
 	{
-	
-		switch(state){
+		spawnPointHandler?.AddCurrentAliveEnemy();
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+
+		switch (state)
+		{
 			case States.Normal:
 				NormalState();
 				break;
@@ -78,20 +83,21 @@ public partial class Enemy : CharacterBody2D
 				ChaseState();
 				break;
 			case States.Attack:
-			{
-				AttackStateAsync();
-				break;
-			}
+				{
+					AttackStateAsync();
+					break;
+				}
 		}
 	}
 
-	public void Move(Vector2 direction){
+	public void Move(Vector2 direction)
+	{
 		Vector2 velocity = Velocity;
 
 		if (direction != Vector2.Zero)
 		{
 			velocity = direction * Speed;
-		
+
 		}
 		else
 		{
@@ -103,55 +109,61 @@ public partial class Enemy : CharacterBody2D
 		MoveAndSlide();
 	}
 
-	public void NormalState(){
+	public void NormalState()
+	{
 		//TODO: Move(Random direction)
 	}
 
-	public void ChaseState(){
-		if(playerTarget == null)
+	public void ChaseState()
+	{
+		if (playerTarget == null)
 			return;
 
-		if(isOnCamera)
+		if (isOnCamera)
 		{
-			if(this.GlobalPosition.DistanceTo(playerTarget.GlobalPosition) <= shootRange)
+			if (this.GlobalPosition.DistanceTo(playerTarget.GlobalPosition) <= shootRange)
 			{
-					ChangeState(States.Attack);
+				ChangeState(States.Attack);
 			}
 		}
 
 		Move(this.GlobalPosition.DirectionTo(playerTarget.GlobalPosition));
 	}
 
-	public async void AttackStateAsync(){
-		if(playerTarget == null)
+	public async void AttackStateAsync()
+	{
+		if (playerTarget == null)
 		{
 			return;
 		}
 
-		if(!isOnCamera || this.GlobalPosition.DistanceTo(playerTarget.GlobalPosition) > shootRange)
+		if (!isOnCamera || this.GlobalPosition.DistanceTo(playerTarget.GlobalPosition) > shootRange)
 		{
 			await ToSignal(GetTree().CreateTimer(0.5f), "timeout");
-			if(this.GlobalPosition.DistanceTo(playerTarget.GlobalPosition) > shootRange)
+			if (this.GlobalPosition.DistanceTo(playerTarget.GlobalPosition) > shootRange)
+			{
 				ChangeState(States.Chase);
+				return;
+			}
 		}
 
-		if(canAttackSubState)
+		if (canAttackSubState)
 		{
 			Random random = new Random();
 
 			canAttackSubState = false;
 			attackSubStateTimer.Start();
 
-			if(random.NextDouble() < 0.5)
+			if (random.NextDouble() < 0.5)
 			{
 				attackState = AttackStates.MoveAttack;
 				double randomN = random.NextDouble();
 
-				if(randomN < 0.25)
+				if (randomN < 0.25)
 					attackMoveDir = Vector2.Left;
-				else if(randomN < 0.5)
+				else if (randomN < 0.5)
 					attackMoveDir = Vector2.Right;
-				else if(randomN < 0.75)
+				else if (randomN < 0.75)
 					attackMoveDir = Vector2.Up;
 				else
 					attackMoveDir = Vector2.Down;
@@ -159,21 +171,23 @@ public partial class Enemy : CharacterBody2D
 			else
 				attackState = AttackStates.ShootAttack;
 		}
-		else{
-			if(attackState == AttackStates.MoveAttack)
+		else
+		{
+			if (attackState == AttackStates.MoveAttack)
 				AttackMoveState();
 		}
-		
 
-		if(attackState == AttackStates.ShootAttack)
+
+		if (attackState == AttackStates.ShootAttack)
 			AttackShootState();
 	}
 
-	public void AttackShootState(){
+	public void AttackShootState()
+	{
 		if (playerTarget == null)
 			return;
 
-		if(canShoot)
+		if (canShoot)
 		{
 			gun.Shoot(this.GlobalPosition.DirectionTo(playerTarget.GlobalPosition));
 			canShoot = false;
@@ -181,48 +195,56 @@ public partial class Enemy : CharacterBody2D
 		}
 	}
 
-	public void AttackMoveState(){
+	public void AttackMoveState()
+	{
 
 		Move(attackMoveDir);
 	}
 
-	public void ShootWaitTimeout(){
+	public void ShootWaitTimeout()
+	{
 		canShoot = true;
 	}
 
-	public void AttackSubStateTimerTimeout(){
+	public void AttackSubStateTimerTimeout()
+	{
 		canAttackSubState = true;
 	}
 
-	public void Hitted(){
+	public void Hitted()
+	{
 		health -= 1;
 
-		if(health <= 0)
+		if (health <= 0)
 			Dead();
 
 	}
 
-	public void Area2DBodyEntered(Node2D body){
-		if(body is Player)
+	public void Area2DBodyEntered(Node2D body)
+	{
+		if (body is Player)
 		{
 			playerTarget = (Player)body;
 			ChangeState(States.Chase);
 		}
 	}
 
-	public void Area2DBodyExited(Node2D body){
-		if(body is Player)
+	public void Area2DBodyExited(Node2D body)
+	{
+		if (body is Player)
 		{
 			playerTarget = null;
 			ChangeState(States.Normal);
 		}
 	}
 
-	public void EnteredCameraViewport(){
+	public void EnteredCameraViewport()
+	{
 		isOnCamera = true;
 	}
 
-	public void ExitedCameraViewport(){
+	public void ExitedCameraViewport()
+	{
 		isOnCamera = false;
 	}
 
@@ -231,10 +253,11 @@ public partial class Enemy : CharacterBody2D
 
 		spawnPointHandler?.SubtractCurrentAliveENemy();
 		QueueFree();
-		
+
 	}
-	
-	private void ChangeState(States newState){
+
+	private void ChangeState(States newState)
+	{
 		state = newState;
 		// await
 	}
